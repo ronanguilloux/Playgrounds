bdca10bd4338134c378b06d22cf9a6f4df09bf51
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-10-30 23:51:49 +0100

    iOS stuff

---
+++ iOS/README.md
@@ -0,0 +1,10 @@
+iOS-playground
+==============
+
+iOS tuts, tests, fails, &amp; other little POCs
+
+ToDoList:
+
+The original https://developer.apple.com tutorial
+
+Next step to do:  http://goo.gl/xvNNjH
---
+++ iOS/ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ronan Guilloux on 27/10/13.
+//  Copyright (c) 2013 Les Polypodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
