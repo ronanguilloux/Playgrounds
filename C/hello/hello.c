394f5cad3570b7112c01c1c21d435bc6dc417434
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-11-02 16:21:01 +0100

    tutorial step 1

---
+++ C/hello/hello.c
@@ -0,0 +1,16 @@
+/*
+ * hello.c
+ * Copyright (C) 2013 ronan <ronan@MacBook-Pro-de-Ronan.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("Hello world!\n");
+    int x = 2+2;
+    return 0;
+}
